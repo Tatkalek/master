bfed9025f8de59ef1df70c7c2b8a333200c56b81
Tatka <dolinyak180199@mail.ru>
2019-09-19 20:52:32 +0400

    master

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout <<"¬ведите двухзначное целое число: "<< endl;
+	cin >> a;
+	cin >> b;
+	return 0;
+}
